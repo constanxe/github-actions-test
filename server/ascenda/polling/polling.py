db052c97ac36aad73a2b2827671104c61bec68c7
Constance <hytan.2018@sis.smu.edu.sg>
2021-04-05 13:36:24 +0800

    github sha fix, test polling

--- server/ascenda/polling/polling.py
+++ server/ascenda/polling/polling.py
@@ -104,6 +104,5 @@ def update_transaction(reference_num, status_code):
         return jsonify({"message": str(e)}), 404
     return "Successfully updated", 201
 
-    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5009, debug=True)
